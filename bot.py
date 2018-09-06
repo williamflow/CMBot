ed08ef751cf215315671f5974ce45d03ded9d7d6
William <william@garden.flow>
2018-09-06 18:04:10 +0200

    Removed chart

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ class Bot:
         self.dispatcher.add_handler(CommandHandler('tarot', self.tarot))
         self.dispatcher.add_handler(CommandHandler('card', self.card))
         self.dispatcher.add_handler(CommandHandler('wheel', self.wheel))
-        self.dispatcher.add_handler(CommandHandler('chart', self.chart))
+        #self.dispatcher.add_handler(CommandHandler('chart', self.chart))
         self.updater.start_polling()
         print "Polling"
     
